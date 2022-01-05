30b7d2c48d323bc21f113d4037a44ad17a5fa03c
Labnan <khalidmasum@iut-dhaka.edu>
2022-01-06 00:06:26 +0600

    Add timer

--- src/Timers.js
+++ src/Timers.js
@@ -4,19 +4,51 @@ import TimerManager from "./TimerInfo";
 
 const timeManager = new TimerManager();
 
+
+
 export default function Timers() {
 
     const [timers, setTimes] = useState(timeManager.getTimers());
     let next = timers.size;
 
-  
+
+
+    function Add() {
+
+        const addTimer = (e) => {
+            e.preventDefault();
+            const newTimer = {
+                name: e.target[0].value,
+                miliseconds: e.target[1].value * 1000
+            }
+
+
+            const newTimers = timers.slice();
+            newTimers.push(newTimer);
+            setTimes(newTimers);
+
+        }
+
+        return <div>
+            <form id="form" onSubmit={addTimer}>
+                <p>Name</p> <input type="text" name="tname"></input>
+                <p>Seconds</p><input type="number" name="ttime"></input>
+                <button>Add</button>
+            </form>
+
+        </div>
+    }
+
+    
+
+
 
     const onTick = (newTime, index, intervalId) => {
         const newTimers = timers.slice();
         console.log(newTimers, index)
         newTimers[index].miliseconds = newTime;
         newTimers[index].intervalId = intervalId;
-        
+
         setTimes(newTimers);
     }
 
@@ -31,7 +63,7 @@ export default function Timers() {
                 {
                     timers.map((timer, index) => {
                         timer.index = index;
-                        return (
+                        return (<>
                             <li key={index}><TimerContainer
                                 info={
                                     {
@@ -39,14 +71,18 @@ export default function Timers() {
                                         onTick
                                     }
                                 }
-                               
+
                             /></li>
+
+                        </>
                         )
                     })
                 }
             </ol>
+            <Add />
 
-            
+
+
         </div>
     );
 }
